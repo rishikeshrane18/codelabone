88c2eaccf52eb6d647e831777e56b7b2321cdddf
rishikeshrane18 <62653435+rishikeshrane18@users.noreply.github.com>
2021-03-28 17:33:14 +0530

    initial commit

---
+++ android/app/src/main/java/com/example/codelabappone/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.codelabappone;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:codelabappone/main.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
+    return MaterialApp(
+      title: "MY NEW APP",
+      home: Scaffold(
+        body: RandomWordsState(),
+        backgroundColor: Colors.blue,
+        ),
+      );
+  }
+}
+
+class RandomWordsState extends StatefulWidget {
+  @override
+  _RandomWordsStateState createState() => _RandomWordsStateState();
+}
+
+class _RandomWordsStateState extends State<RandomWordsState> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 28);
+  @override
+  Widget build(BuildContext context) {
+   // final wordPair = WordPair.random();
+    //return Text(wordPair.asPascalCase);
+    return Scaffold (                     // Add from here...
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: _buildSuggestions(),
+    );
+
+  }
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+
+          // The syntax "i ~/ 2" divides i by 2 and returns an
+          // integer result.
+          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          // This calculates the actual number of word pairings
+          // in the ListView,minus the divider widgets.
+          final int index = i ~/ 2;
+          // If you've reached the end of the available word
+          // pairings...
+          if (index >= _suggestions.length) {
+            // ...then generate 10 more and add them to the
+            // suggestions list.
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+
+      ),
+      trailing: Icon(   // NEW from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      // ... to here.
+      onTap: () {      // NEW lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+}
